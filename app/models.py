d8850ed48848c7467394e5bd94f14c05c4126893
NurmurodovCoder <nurmurodovruslan2004@gmail.com>
2023-11-07 13:08:38 +0500

    test=2

--- app/models.py
+++ app/models.py
@@ -32,9 +32,12 @@ class Banner(models.Model):
 
      def __str__(self):
           return self.title
+     
+     
 
 
 class Post(models.Model):
+     
      title = models.CharField(max_length=100)
      mini_text = models.CharField(max_length=256)
      text = models.TextField()
